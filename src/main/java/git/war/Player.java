d9bbd84d3930a8ef34e3ba85a012f075b476fca1
DColeman4268 <DGRColeman44@gmail.com>
2025-10-08 23:46:25 -0400

    Version 1 of player, created the ability for player to be initialized by the constructor and able to get the name of the player using getName.

---
+++ src/main/java/git/war/Player.java
@@ -0,0 +1,17 @@
+package git.war;
+
+
+public class Player {
+    // The player's name
+    private final String name;
+    
+    // Constructor: initializes the player with a name
+    public Player(String name){
+        this.name = name;
+    }
+    
+    // Returns the player's name
+    public String getName() {
+        return name;
+    }
+}
